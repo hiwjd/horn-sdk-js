6525b35b8fb1c71a8336f695befed4fac5ceb28f
hiwjd <self@hiwjd.com>
2016-12-18 17:26:37 +0800

    增加multiline,TemplateEngine

--- src/utils.js
+++ src/utils.js
@@ -119,6 +119,39 @@ window.HORN = {};
         var k = murmurhash3_32_gc(s,s);
         console.log(k);
     }
+
+    var reCommentContents = /\/\*!?(?:\@preserve)?[ \t]*(?:\r\n|\n)([\s\S]*?)(?:\r\n|\n)[ \t]*\*\//;
+    
+    function multiline(fn) {
+        if (typeof fn !== 'function') {
+            throw new TypeError('Expected a function');
+        }
+
+        var match = reCommentContents.exec(fn.toString());
+
+        if (!match) {
+            throw new TypeError('Multiline comment missing.');
+        }
+
+        return match[1];
+    }
+
+    var TemplateEngine = function(html, options) {
+        var re = /<%([^%>]+)?%>/g, reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g, code = 'var r=[];\n', cursor = 0;
+        var add = function(line, js) {
+            js? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
+                (code += line != '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
+            return add;
+        }
+        var match;
+        while(match = re.exec(html)) {
+            add(html.slice(cursor, match.index))(match[1], true);
+            cursor = match.index + match[0].length;
+        }
+        add(html.substr(cursor, html.length - cursor));
+        code += 'return r.join("");';
+        return new Function(code.replace(/[\r\t\n]/g, '')).apply(options);
+    }
     
     HORN.utils = {
         extends: function(subClass, superClass) {
@@ -131,6 +164,8 @@ window.HORN = {};
         isObject: isObject,
         isFunction: isFunction,
         getOS: getOS,
-        getBrowser: getBrowser
+        getBrowser: getBrowser,
+        multiline: multiline,
+        TE: TemplateEngine
     };
 })(window, window.HORN);
